70fa4083c7ea913c784d471072186848b06ee2fe
Ben Rochford <ben@Bens-MacBook-Pro.local>
2023-10-31 15:34:03 -0400

    added username / logout to app bar, updated login flow

--- youbuddy/lib/main.dart
+++ youbuddy/lib/main.dart
@@ -53,6 +53,15 @@ class _InitializationWidgetState extends State<InitializationWidget> {
     return clientId;
   }
 
+  void _logout() async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.remove('clientId');
+    // restart the app to get a new clientId
+    Navigator.of(context).pushReplacement(MaterialPageRoute(
+      builder: (context) => InitializationWidget(),
+    ));
+  }
+
   Future<String> _getClientIdFromUser(BuildContext context) async {
     final TextEditingController _clientIdController = TextEditingController();
     String? clientId;
@@ -61,27 +70,21 @@ class _InitializationWidgetState extends State<InitializationWidget> {
       barrierDismissible: false,
       builder: (BuildContext context) {
         return AlertDialog(
-          title: Text('Choose a username'),
+          title: Text('Login'),
           content: SingleChildScrollView(
             child: TextField(
               controller: _clientIdController,
               decoration: InputDecoration(
-                labelText: 'Choose a username',
+                labelText: 'Provide username',
               ),
             ),
           ),
           actions: <Widget>[
             TextButton(
-              child: Text('Cancel'),
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-            ),
-            TextButton(
               child: Text('YES!'),
               onPressed: () {
                 clientId = _clientIdController.text;
-                if (clientId != null) {
+                if (clientId != null && clientId!.isNotEmpty) {
                   Navigator.of(context).pop(clientId);
                 }
               },
@@ -112,7 +115,20 @@ class _InitializationWidgetState extends State<InitializationWidget> {
                     Tab(text: 'Friends'),
                   ],
                 ),
-                title: Text('YouBuddies'),
+                title: Text(
+                  'YouBuddies',
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                actions: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                    child: Center(child: Text('[$clientId]')),
+                  ),
+                  TextButton(
+                    onPressed: _logout,
+                    child: Text('logout'),
+                  ),
+                ],
               ),
               body: TabBarView(
                 children: [
