1b93592ea7e5662b4b012f3f99510ec7d6b9615d
Ben Rochford <ben@Bens-MacBook-Pro.local>
2023-11-02 14:31:56 -0400

    removed thumbnail collection from extension, doesn't work

--- chrome_extension/background.js
+++ chrome_extension/background.js
@@ -7,20 +7,17 @@ function collectRecommendations() {
     recommendationElements.forEach((element) => {
       const titleElement = element.querySelector('.style-scope ytd-rich-grid-media a#video-title-link');
       const channelElement = element.querySelector('.style-scope ytd-rich-grid-media ytd-channel-name #text');
-      const thumbnailElement = element.querySelector('.style-scope ytd-rich-grid-media ytd-thumbnail img');
       
       const title = titleElement ? titleElement.innerText : 'N/A';
       const link = titleElement ? titleElement.href.split('&')[0] : 'N/A';
       const channel = channelElement ? channelElement.innerText : 'N/A';
-      const thumbnail = thumbnailElement ? thumbnailElement.src : 'N/A';
 
       if (title !== 'N/A' && !addedTitles.has(title)) {
         addedTitles.add(title);
         recommendations.push({
           title,
           link,
-          channel,
-          thumbnail
+          channel
         });
       }
     });
