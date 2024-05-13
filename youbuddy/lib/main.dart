7695d60c0a08ef347a91187e06412fbaa99334d6
Sidney Wright <swright3743@gmail.com>
2024-05-13 01:27:49 -0400

    Cloud functions on new user and at midnight

--- youbuddy/lib/main.dart
+++ youbuddy/lib/main.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 import 'dart:html';
 import 'dart:js' as js;
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:cloud_functions/cloud_functions.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:youbuddy/query_route_generator.dart';
 import 'firebase_options.dart';
@@ -25,6 +26,10 @@ void main() async {
     options: DefaultFirebaseOptions.currentPlatform,
   );
 
+  FirebaseFunctions.instance.useFunctionsEmulator('localhost', 5001);
+  FirebaseFirestore.instance.useFirestoreEmulator('localhost', 8080);
+  FirebaseAuth.instance.useAuthEmulator('localhost', 9099);
+      
   usePathUrlStrategy();
   runApp(MyApp());
 }
