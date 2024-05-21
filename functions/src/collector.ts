9c4b1dc1b743f44a38805359d132871b8598838e
Sidney Wright <swright3743@gmail.com>
2024-05-21 00:44:38 -0400

    update logging spacing, simplify promise chaining, add importable collector file

---
+++ functions/src/collector.ts
@@ -0,0 +1,213 @@
+/**
+ * Import function triggers from their respective submodules:
+ *
+ * import {onCall} from "firebase-functions/v2/https";
+ * import {onDocumentWritten} from "firebase-functions/v2/firestore";
+ *
+ * See a full list of supported triggers at https://firebase.google.com/docs/functions
+ */
+
+// Start writing functions
+// https://firebase.google.com/docs/functions/typescript
+
+// The Firebase Admin SDK to access Firestore.
+import serviceAccount from "../firebase-adminsdk-key.json";
+import {credential} from "firebase-admin";
+import {AppOptions, initializeApp} from "firebase-admin/app";
+import {getFirestore, FieldValue} from "firebase-admin/firestore";
+
+const cert = {
+  projectId: serviceAccount.project_id,
+  clientEmail: serviceAccount.client_email,
+  privateKey: serviceAccount.private_key
+};
+
+// Initialize Firebase
+const firebaseConfig: AppOptions = {
+  projectId: "youbuddy-96438",
+  databaseURL: "https://youbuddy-96438-default-rtdb.firebaseio.com",
+  credential: credential.cert(cert)
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+// Config from Google Cloud Project
+const googleOAuthConfig = {
+  clientSecret: "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",
+  clientId: "963863199423-gq6l1ur7gtgg9li2o124j5hrn96th2c4.apps.googleusercontent.com",
+};
+
+/** Retrieve access tokens by exchanging refresh tokens in firebase
+ * Google OAuth implementation can be seen at https://developers.google.com/identity/protocols/oauth2
+ */
+async function getAllAccessTokens() {
+  const snapshot = await db.collection("tokens").get();
+  const docs: any[] = [];
+  snapshot.forEach((doc) => docs.push(doc));
+  const accessTokens: Record<string, string> = {};
+
+  for (const doc of docs) {
+    const userId = doc.id;
+    const refreshToken = doc.refreshToken;
+
+    accessTokens[userId] = await getAccessToken(userId, refreshToken) || "";
+  }
+
+  return accessTokens;
+}
+
+async function getAccessToken(userId: string, refreshToken?: string): Promise<string | null> {
+  const googleOAuthEndpoint = new URL("/token", "https://oauth2.googleapis.com");
+  googleOAuthEndpoint.searchParams.set("client_secret", googleOAuthConfig.clientSecret);
+  googleOAuthEndpoint.searchParams.set("client_id", googleOAuthConfig.clientId);
+  googleOAuthEndpoint.searchParams.set("grant_type", "refresh_token");
+
+  if (!refreshToken) {
+    const snapshot = await db.collection("tokens").doc(userId).get();
+    const doc = snapshot.data();
+    if (doc === undefined) {
+      console.error(`Error retrieving token from db for ${userId}`);
+      return null;
+    }
+
+    refreshToken = doc.refreshToken;
+  }
+
+  googleOAuthEndpoint.searchParams.set("refresh_token", <string>refreshToken);
+  const response = await fetch(googleOAuthEndpoint, {
+    method: "POST",
+  });
+
+  const data = await response.json();
+  if (response.ok) {
+    console.log(`Access token retrieved for ${userId}`);
+    return data.access_token;
+  } else {
+    console.error(`Error retrieving access token for ${userId}: ${JSON.stringify(data)}`);
+    return null;
+  }
+}
+
+function storeData(userId: string, recommendations: any[], filterChipTexts: string[]) {
+  // yyyy_MM_dd_HH_mm_ss for document name
+  const date = new Date();
+  const datetimeString = date.toISOString().replace(/-|:/g, "_").replace(/\.\d{3}Z/g, "");
+
+  return (
+    db.collection("users")
+      .doc(userId)
+      .collection("youtubeRecommendations")
+      .doc(datetimeString) // Using datetime with underscores as document ID
+      .set({ // Send recommendations to database
+        recommendations: recommendations,
+        topics: filterChipTexts,
+        timestamp: FieldValue.serverTimestamp(),
+      })
+  );
+}
+
+const excluded_chips = ["N/A", "All", "Recently uploaded", "Watched", "New to you"];
+
+async function collectRecsAllUsers(accessTokens: Record<string, string>) {
+  const data: Record<string, any> = {};
+  for (const [userId, accessToken] of Object.entries(accessTokens)) {
+    data[userId] = await collectRecs(userId, accessToken);
+  }
+
+  return data;
+}
+
+async function collectRecs(userId: string, accessToken: string): Promise<{recommendations: any[], filterChipTexts: string[]}> {
+  const youtubeUrl = "https://www.youtube.com";
+  const browseEndpoint = "/youtubei/v1/browse";
+  const browseUrl = new URL(browseEndpoint, youtubeUrl);
+  const browseRequest = {
+    "context": {
+      "client": {
+        "clientName": "WEB",
+        "clientVersion": "2.20231101.05.00",
+      },
+    },
+    "browseId": "FEwhat_to_watch",
+  };
+
+  const recommendations: any[] = [];
+  const addedRecs = new Set();
+  const filterChipTexts: string[] = [];
+  const response = await fetch(browseUrl, {
+    method: "POST",
+    headers: {
+      "Authorization": "Bearer " + accessToken,
+    },
+    body: JSON.stringify(browseRequest),
+  });
+ 
+  if (response.ok) {
+    const body = await response.json();
+    const richGridRenderer = body?.contents?.twoColumnBrowseResultsRenderer?.tabs[0]?.tabRenderer?.content?.richGridRenderer;
+    const items = richGridRenderer?.contents || [];
+    const filterChips = richGridRenderer?.header?.feedFilterChipBarRenderer?.contents || [];
+    const youtubeWatchUrl = "https://www.youtube.com/watch?v=";
+
+    // get videos
+    for (const item of items) {
+      // ad items have adSlotRenderer and items to trigger next query have continuationItemRenderer
+      const video = item?.richItemRenderer?.content?.videoRenderer;
+
+      // ensure real video
+      if (video) {
+        const newRec = {
+          "title": video?.title?.runs[0]?.text || "<no title found>",
+          "link": youtubeWatchUrl + video?.videoId,
+          "channel": video?.ownerText?.runs[0]?.text || "<no channel found>",
+        };
+
+        if (!addedRecs.has(JSON.stringify(newRec))) {
+          recommendations.push(newRec);
+          addedRecs.add(JSON.stringify(newRec));
+        }
+      }
+    }
+
+    // get filter chips
+    for (const chip of filterChips) {
+      const text = chip?.chipCloudChipRenderer?.text?.runs[0]?.text;
+      if (text && !excluded_chips.includes(text)) {
+        filterChipTexts.push(text);
+      }
+    }
+
+
+    return {recommendations, filterChipTexts};
+  } else {
+    console.error(`Error retrieving recommendations from innertube browse endpoint: ${await response.text()}`);
+    return {recommendations, filterChipTexts};
+  }
+}
+
+export async function collectRecsUser(userId: string) {
+   console.log(new Date().toUTCString());
+   const accessToken = await getAccessToken(userId);
+   if (accessToken) {
+     const data = await collectRecs(userId, accessToken);
+     storeData(userId, data.recommendations, data.filterChipTexts)
+       .then(console.log(`Document ${userId} successfully written!\n`))
+       .catch(error => console.error(`Error adding document ${userId}: `, error, '\n'));
+   }
+}
+
+export async function collectAllRecs() {
+  console.log(new Date().toUTCString());
+  getAllAccessTokens().then(accessTokens => {
+    collectRecsAllUsers(accessTokens).then(data => {
+      const promises: Promise[] = Object.entries(data).map(([userId, userData]) => 
+        storeData(userId, userData.recommendations, userData.filterChipTexts)
+          .then(console.log(`Document ${userId} successfully written!`))
+          .catch(error => console.error(`Error adding document ${userId}: `, error)));
+      
+      Promise.all(promises).then(console.log());
+    });
+  });
+}
+
